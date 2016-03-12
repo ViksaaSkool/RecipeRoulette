31822336d056d9f71c6ccb7d103281bdb22b675e
Viktor Arsovski <viktor.arsovski@propeople.dk>
2016-03-12 21:12:07 +0100

    Initial commit

---
+++ app/src/main/java/com/recipe/roulette/app/RecipeRouletteApplication.java
@@ -0,0 +1,14 @@
+package com.recipe.roulette.app;
+
+import android.app.Application;
+
+/**
+ * Created by varsovski on 12-Mar-16.
+ */
+public class RecipeRouletteApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
