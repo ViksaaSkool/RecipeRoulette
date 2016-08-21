a71bd4c9cc903b07b4b3c76a82768a98f73f42b9
Viktor Arsovski <viktor.arsovski@propeople.dk>
2016-08-21 13:40:36 +0200

    dependencies update

---
+++ app/src/main/java/com/recipe/roulette/app/ui/activity/SplashScreenActivity.java
@@ -0,0 +1,20 @@
+package com.recipe.roulette.app.ui.activity;
+
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+/**
+ * Created by varsovski on 26-May-16.
+ */
+public class SplashScreenActivity extends AwesomeSplash {
+
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+
+    }
+
+    @Override
+    public void animationsFinished() {
+
+    }
+}
