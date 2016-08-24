100e2a01dd8e1eab8a4b6d4368e5d52e5eb29d46
Viktor Arsovski <viktor.arsovski@propeople.dk>
2016-08-25 01:35:27 +0200

    ui pt2

---
+++ app/src/main/java/com/recipe/roulette/app/adapters/Cl.java
@@ -0,0 +1,7 @@
+package com.recipe.roulette.app.adapters;
+
+/**
+ * Created by varsovski on 25-Aug-16.
+ */
+public class Cl {
+}
---
+++ app/src/main/java/com/recipe/roulette/app/adapters/recycler/BaseRecyclerAdapter.java
@@ -0,0 +1,7 @@
+package com.recipe.roulette.app.adapters.recycler;
+
+/**
+ * Created by varsovski on 25-Aug-16.
+ */
+public class BaseRecyclerAdapter {
+}
